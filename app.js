2e0a964bb7b62f0e90194c9beb3ac998db536c63
Roselma Mendes <roselma.mendes@gmail.com>
2016-04-05 10:11:40 -0300

    Dojo no dia 05/04/16.

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 module.exports = {
-  hello: function() {
-    return "it's me.";
+  toArabic: function(romanNumeral) {
+    var romans = {'I': 1, 'V': 5, 'X': 10};
+    var result = 0;
+    var resultRoman = romanNumeral.split('');
+    var currentNumber = 0;
+    resultRoman.forEach(function(char){
+      if (currentNumber >= romans[char]){ 
+        result += romans[char]; } 
+      else {
+        result -= romans[char];
+      }
+      currentNumber = romans[char];
+    })
+    return result;
   }
 }
--- test.js
+++ test.js
@@ -1,11 +1,32 @@
 var assert = require('assert');
-var Song = require('./app.js');
+var RomanNumeral = require('./app.js');
 
-describe('Song', function() {
+describe('RomanNumeral', function() {
   
-    it('should return Adele Song when it call hello method', function () {
+    it('should return 1 when it receives I', function () {
       
-      assert.equal("it's me.", Song.hello());
+      assert.equal(1, RomanNumeral.toArabic("I"));
     });
+  
+  it('should return 5 when it receives    V', function () {
+      
+      assert.equal(5, RomanNumeral.toArabic("V"));
+    });
+  
+  it('should return 2 when it receives    II', function () {
+      
+      assert.equal(2, RomanNumeral.toArabic("II"));
+    });
+  
+  it('should return 10 when it receives X', function() {
+    
+    assert.equal(10,
+    RomanNumeral.toArabic("X"));
+  })
+  
+  it('should return 4 when it receives IV', function(){
+    assert.equal(4, 
+    RomanNumeral.toArabic("IV"));
+  })
 
 });
